091bcb1f1e233cf5790e843ac3e2ad4931f82722
Adrian Riobo <adrian.riobo.lorenzo@gmail.com>
2020-08-06 03:10:14 +0000

    Fix test setup-target script to add the issuer CA (only crt for external ca mode)

--- scripts/setup-target.sh
+++ scripts/setup-target.sh
@@ -3,21 +3,22 @@
 CURRENT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 TARGET=$CURRENT/../target
 NODENAME=localhost.localdomain
+EXTERNAL_CA=$CURRENT/../pki-mock/ca.crt
 
 # kubelet
-sudo cp $CURRENT/../target/kubernetes/pki/kubelet-client-$NODENAME.crt /etc/kubernetes/pki/kubelet-client.crt
-sudo cp $CURRENT/../target/kubernetes/pki/kubelet-client-$NODENAME.key /etc/kubernetes/pki/kubelet-client.key
-sudo cp $CURRENT/../target/kubernetes/pki/kubelet-server-$NODENAME.crt /etc/kubernetes/pki/kubelet-server.crt
-sudo cp $CURRENT/../target/kubernetes/pki/kubelet-server-$NODENAME.key /etc/kubernetes/pki/kubelet-server.key
+sudo cp $TARGET/kubernetes/pki/kubelet-client-$NODENAME.crt /etc/kubernetes/pki/kubelet-client.crt
+sudo cp $TARGET/kubernetes/pki/kubelet-client-$NODENAME.key /etc/kubernetes/pki/kubelet-client.key
+sudo cp $TARGET/kubernetes/pki/kubelet-server-$NODENAME.crt /etc/kubernetes/pki/kubelet-server.crt
+sudo cp $TARGET/kubernetes/pki/kubelet-server-$NODENAME.key /etc/kubernetes/pki/kubelet-server.key
 
 # apiserver
 
-sudo cp $CURRENT/../target/kubernetes/pki/apiserver-$NODENAME.crt /etc/kubernetes/pki/apiserver.crt
-sudo cp $CURRENT/../target/kubernetes/pki/apiserver-$NODENAME.key /etc/kubernetes/pki/apiserver.key
-sudo cp $CURRENT/../target/kubernetes/pki/apiserver-etcd-client.crt /etc/kubernetes/pki/apiserver-etcd-client.crt
-sudo cp $CURRENT/../target/kubernetes/pki/apiserver-etcd-client.key /etc/kubernetes/pki/apiserver-etcd-client.key
-sudo cp $CURRENT/../target/kubernetes/pki/apiserver-kubelet-client.crt /etc/kubernetes/pki/apiserver-kubelet-client.crt
-sudo cp $CURRENT/../target/kubernetes/pki/apiserver-kubelet-client.key /etc/kubernetes/pki/apiserver-kubelet-client.key
+sudo cp $TARGET/kubernetes/pki/apiserver-$NODENAME.crt /etc/kubernetes/pki/apiserver.crt
+sudo cp $TARGET/kubernetes/pki/apiserver-$NODENAME.key /etc/kubernetes/pki/apiserver.key
+sudo cp $TARGET/kubernetes/pki/apiserver-etcd-client.crt /etc/kubernetes/pki/apiserver-etcd-client.crt
+sudo cp $TARGET/kubernetes/pki/apiserver-etcd-client.key /etc/kubernetes/pki/apiserver-etcd-client.key
+sudo cp $TARGET/kubernetes/pki/apiserver-kubelet-client.crt /etc/kubernetes/pki/apiserver-kubelet-client.crt
+sudo cp $TARGET/kubernetes/pki/apiserver-kubelet-client.key /etc/kubernetes/pki/apiserver-kubelet-client.key
 
 # etcd
 
@@ -31,15 +32,20 @@ sudo cp $TARGET/kubernetes/pki/etcd/server-$NODENAME.key /etc/kubernetes/pki/etc
 
 # sa
 
-sudo cp $CURRENT/../target/kubernetes/pki/sa.key /etc/kubernetes/pki/sa.key
-sudo cp $CURRENT/../target/kubernetes/pki/sa.pub /etc/kubernetes/pki/sa.pub
+sudo cp $TARGET/kubernetes/pki/sa.key /etc/kubernetes/pki/sa.key
+sudo cp $TARGET/kubernetes/pki/sa.pub /etc/kubernetes/pki/sa.pub
 
 # config files
 
-sudo cp $CURRENT/../target/kubernetes/kubelet-$NODENAME.conf /etc/kubernetes/kubelet.conf
-sudo cp $CURRENT/../target/kubernetes/admin-$NODENAME.conf /etc/kubernetes/admin.conf
-sudo cp $CURRENT/../target/kubernetes/controller-manager-$NODENAME.conf /etc/kubernetes/controller-manager.conf
-sudo cp $CURRENT/../target/kubernetes/scheduler-$NODENAME.conf /etc/kubernetes/scheduler.conf
+sudo cp $TARGET/kubernetes/kubelet-$NODENAME.conf /etc/kubernetes/kubelet.conf
+sudo cp $TARGET/kubernetes/admin-$NODENAME.conf /etc/kubernetes/admin.conf
+sudo cp $TARGET/kubernetes/controller-manager-$NODENAME.conf /etc/kubernetes/controller-manager.conf
+sudo cp $TARGET/kubernetes/scheduler-$NODENAME.conf /etc/kubernetes/scheduler.conf
+
+# copy external ca
+
+sudo cp $EXTERNAL_CA /etc/kubernetes/pki
+sudo cp $EXTERNAL_CA /etc/kubernetes/pki/etcd
 
 # Check if required
 sudo chown -R root:root /etc/kubernetes/pki
